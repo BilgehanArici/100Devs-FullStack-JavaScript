e44bf58a78a304594924e012a6037456a2b84812
Bilgehan Arici <bilgehanarici97@gmail.com>
2022-03-01 18:13:39 +0300

    js first lesson color picker added

--- Week6/class12/class12-materials/background-picker/js/main.js
+++ Week6/class12/class12-materials/background-picker/js/main.js
@@ -1,19 +1,27 @@
 document.getElementById('purple').onclick = partyPurple
 document.getElementById('green').onclick = partyGreen
 document.getElementById('blue').onclick = partyBlue
+document.getElementById('orange').onclick = partyOrange
 
+// event listener
+// document.getElementById('IdName').EventName (onclick) = functionName (partyBlue)
+// go to document => find element by the id of... => onclick = run a function 
+// event listener
 
 function partyPurple() {
-  document.querySelector('body').style.backgroundColor = 'rgba(241,63,247,1)'
+  document.querySelector('body').style.backgroundColor = ('rgba(241,63,247,1)') 
   document.querySelector('body').style.color = 'white'
 }
-
 function partyGreen() {
-  document.querySelector('body').style.backgroundColor = 'rgba(0,253,81,1)'
+  document.querySelector('body').style.backgroundColor = ('rgba(0,253,81,1')
+  document.querySelector('body').style.color = 'white'
+}
+function partyBlue() {
+  document.querySelector('body').style.backgroundColor = ('rgba(0,254,255)')
+  document.querySelector('body').style.color = 'white'
+}
+function partyOrange() {
+  document.querySelector('body').style.backgroundColor = ('orange')
   document.querySelector('body').style.color = 'white'
 }
 
-function partyBlue() {
-  document.querySelector('body').style.backgroundColor = 'rgba(0,254,255)'
-  document.querySelector('body').style.color = 'white'
-}
