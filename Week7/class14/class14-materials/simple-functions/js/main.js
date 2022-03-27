094108a2faaf812a7c157e28440f9e8178f4eb75
Bilgehan Arici <bilgehanarici97@gmail.com>
2022-03-27 13:11:31 +0300

    class 14 finished

--- Week7/class14/class14-materials/simple-functions/js/main.js
+++ Week7/class14/class14-materials/simple-functions/js/main.js
@@ -1,5 +1,21 @@
 //create a function that adds two numbers and alerts the sum
+function addTwoNumbers(num1,num2){
+    alert(num1+num2)
+}
+
+addTwoNumbers(2,3)
 
 //create a function that multiplys three numbers and console logs the product
+function multiplyThreeNumbers(num1,num2,num3){
+    const multiplication = num1*num2*num3
+    console.log(multiplication)
+}
+multiplyThreeNumbers(2,3,4)
 
-//create a function that divides two numbers and returns the ???
+//create a function that divides two numbers and returns the quotient
+function dividesTwoNumbers(num1,num2){
+    return num1/num2
+}
+
+console.log(dividesTwoNumbers(5,3))
+// return spits out the value to where its called
