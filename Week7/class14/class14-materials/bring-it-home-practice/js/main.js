dfa2f0aa26c763d87c064aeeae278dfc93ca4169
Bilgehan Arici <bilgehanarici97@gmail.com>
2022-04-02 21:33:25 +0300

    class 15 hw finished

--- Week7/class14/class14-materials/bring-it-home-practice/js/main.js
+++ Week7/class14/class14-materials/bring-it-home-practice/js/main.js
@@ -23,5 +23,22 @@ console.log(remainder(4,3))
 
 // *Conditionals*
 // Create a function that adds two numbers and if the sum is greater than 50 alert Jumanji
+function jumanji(num1,num2){
+    const sum = num1+num2 
+    if (sum>50){
+        alert('jumanji')
+    }
+}
+
+jumanji(25,26)
 
 // Create a function that multiplys three numbers and if the product is divisible by 3 alert ZEBRA
+
+function multiplyTrio(num1,num2,num3){
+    const multp = num1*num2*num3
+    if(multp%3==0){
+        alert('zebra')
+    } 
+}
+
+multiplyTrio(1,2,3)
