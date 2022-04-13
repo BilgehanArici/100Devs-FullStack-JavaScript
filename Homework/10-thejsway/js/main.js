0ae1664ff2f632f5d98273bd4ed53654927e5baa
Bilgehan Arici <bilgehanarici97@gmail.com>
2022-04-13 21:16:27 +0300

    jsway hw started

---
+++ Homework/10-thejsway/js/main.js
@@ -0,0 +1,103 @@
+// let Age = 24
+// let Name = 'Bilgehan Arici'
+
+// console.log(Age)
+// console.log(Name)
+
+// function simpleCalculator(n1,n2){
+//     const sum = n1+n2
+//     const sub = n1-n2
+//     const multip = n1*n2
+//     const div = n1/n2
+//     console.log(sum)
+//     console.log(sub)
+//     console.log(multip)
+//     console.log(div)
+// }
+
+// simpleCalculator(6,3)
+
+// console.log(4 + 5);
+// console.log("4 + 5");
+// console.log("4" + "5");
+
+
+// const name = prompt('enter your name')
+// alert(`hello ${name}`)
+
+// const firstName = prompt('what is your first name')
+// const lastName = prompt('what is your last name')
+// alert(`hello ${firstName} ${lastName}`)
+
+// 2
+// 10
+// 102
+// 30
+// 40
+// 10
+// 10/4
+
+// let a = 2;
+// a -= 1;
+// a++;
+// let b = 8;
+// b += 2;
+// const c = a + b * b;
+// const d = a * b + b;
+// const e = a * (b + b);
+// const f = a * b / a;
+// const g = b / a * a;
+// console.log(a, b, c, d, e, f, g);
+
+// let rawPrice = Number(prompt('give me the RAW price'))
+
+// let finalPrice = (rawPrice*120.6/100)
+// alert(finalPrice)
+
+// let celcius = Number(prompt('give me celcius'))
+
+// let fahrenheit = (celcius*9/5)+32
+
+// alert(fahrenheit)
+
+// let number1 = 5;
+// let number2 = 3;
+
+// // TODO: type your code here (and nowhere else!)
+// let temp 
+// temp = number1
+// number1 = number2
+// number2 = temp
+
+
+
+// //////////////////////////////////////
+
+// console.log(number1); // Should show 3
+// console.log(number2); // Should show 5
+// //This exercise has several valid solutions. You may use more than two variables to solve it.
+
+// const number = Number(prompt('enter a number'))
+// if (number>0){Mon
+//     alert('number is positive')
+// }
+
+// const day = prompt('gimme a day').toLowerCase()
+// switch (day) {
+//     case 'monday':
+//         alert('tuesday')
+//         break
+//     case 'tuesday':
+//         alert('wednesday')
+//         break
+//     case 'wednesday':
+//         alert('thursday')
+// }
+
+// if (day == 'thursday'){
+//     alert('friday')
+// }else if (day == 'friday'){
+//     alert('saturday')
+// }else if (day== 'saturday'){
+//     alert('sunday')
+// }
