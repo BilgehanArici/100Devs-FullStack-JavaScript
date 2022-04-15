a37db886e8e09e6ced6f4b4c234e137e63c69dec
Bilgehan Arici <bilgehanarici97@gmail.com>
2022-04-15 12:31:34 +0300

    jsway hw is FINALLY finished

--- Homework/10-thejsway/js/main.js
+++ Homework/10-thejsway/js/main.js
@@ -244,12 +244,185 @@
 
 // Neither yes nor no
 // Write a program that plays 
-//"neither yes, nor no" with the user. Specifically, 
-//the programs asks the user to enter text until either "yes" or "no" is typed, which ends the game.
-
+// //"neither yes, nor no" with the user. Specifically, 
+// the programs asks the user to enter text until either "yes" or "no" is typed, which ends the game.
+// let answer = prompt('give an answer')
+// while (answer!= 'no' && answer!= 'yes'){
+//     answer = prompt('give an answer')
+// }
 //ask the user for an answer
 
 // if the answer is yes or no finish the program
 
 // if not keep asking
 
+//FizzBuzz
+// Write a program that shows all numbers between 1 and 100 with the following exceptions:
+
+// It shows "Fizz" instead if the number is divisible by 3.
+
+// It shows "Buzz" instead if the number is divisible by 5 and not by 3.
+
+// When it's done, improve it so that the program shows "FizzBuzz" instead for numbers divisible both by 3 and by 5.
+
+// for (i=1;i<=100;i++){
+//     if(i%3==0 && i%5==0){
+//         console.log(`${i} FizzBuzz`)
+//     }else if (i%3==0){
+//         console.log(`${i} Fizz`) 
+//     }else if (i%5==0){
+//         console.log(`${i} Buzz`)
+//     }else{
+//         console.log(i)
+//     }
+// }
+
+// function squareRoot(n){
+//     const returnValue = n*n
+//     return returnValue
+//     console.log('hi')
+// }
+
+// const result = squareRoot(2)
+// console.log(result)
+
+// function presentation(name, age) {
+//     console.log(`Your name is ${name} and you're ${age} years old`);
+//   }
+
+//   presentation("Garance"); // "Your name is Garance and you're 9 years old"
+//   presentation(5, "Prosper"); // "Your name is 5 and you're Prosper years old"
+
+//   const hello = function(name) {
+//     const message = `Hello, ${name}!`;
+//     return message;
+//   };
+
+//   console.log(hello("Richard")); // "Hello, Richard!"
+
+// const hello = (name) => {
+//     console.log(`hello ${name}`)
+// }
+
+// hello ('bigo')
+
+// Improved hello
+// Complete the following program so that it asks the user for his first and last names, then show the result of the sayHello() function.
+
+// // Say hello to the user
+// function sayHello(firstName, lastName) {
+//     const message = `Hello, ${firstName} ${lastName}!`;
+//     return message;
+//   }
+  
+//   firstName = prompt('first name')
+//   lastName = prompt('last name')
+//   alert(sayHello(firstName,lastName))
+
+
+  
+// // Square the given number x
+// function square1(x) {
+//   return total = x*x
+// }
+
+// // Square the given number x
+// const square2 = x => {
+//     return total = x*x
+// }
+
+// console.log(square1(0)); // Must show 0
+// console.log(square1(2)); // Must show 4
+// console.log(square1(5)); // Must show 25
+
+// console.log(square2(0)); // Must show 0
+// console.log(square2(2)); // Must show 4
+// console.log(square2(5)); // Must show 25
+
+// When it's done, update the program so that it shows the square of every number between 0 and 10.
+
+// function square(x) {
+//   return total = x*x
+// }
+
+// for(i=1;i<=10;i++){
+//     console.log(`${i} square = ${square(i)}`)
+// }
+
+// Writing 10 dumb calls to square() is forbidden! You know how to repeat statements, don't you? ;)
+
+// Minimum of two numbers
+// Let's pretend the JavaScript Math.min() function doesn't exist. Complete the following program so that the min() function returns the minimum of its two received numbers.
+
+// // TODO: write the min() function
+
+// console.log(min(4.5, 5)); // Must show 4.5
+// console.log(min(19, 9));  // Must show 9
+// console.log(min(1, 1));   // Must show 1
+
+// Minimum of two numbers
+// Let's pretend the JavaScript Math.min() function doesn't exist. Complete the following program so that the min() function returns the minimum of its two received numbers.
+
+// function min(n1,n2){
+//     if(n1>n2){
+//         return(n2)
+//     }else if (n1<n2){
+//         return(n1)
+//     }else{
+//         return(n1)
+//     }
+// } 
+
+// console.log(min(4.5, 5)); // Must show 4.5
+// console.log(min(19, 9));  // Must show 9
+// console.log(min(1, 1));   // Must show 1
+
+// Calculator
+// Complete the following program so that it offers the four basic arithmetical operations: addition, subtraction, multiplication and division. You can use either a function declaration or a function expression.
+
+// // TODO: complete program
+// function add(n1,n2){
+//     total = n1+n2
+//     return total
+// }
+// function subtract(n1,n2){
+//     total = n1-n2
+//     return total
+// }
+// const multiply = function(n1,n2){
+//     total = n1*n2
+//     return total
+// }
+// const divide = (n1,n2) => {
+//     total = n1/n2
+//     return total
+// }
+
+// console.log(add(4, 6));  // Must show 10
+// console.log(subtract(4, 6));  // Must show -2
+// console.log(multiply(2, 0));  // Must show 0
+// console.log(divide(12, 0)); // Must show Infinity
+
+
+// Circumference and area of a circle
+// Write a program containing two functions to calculate the circumference and area of a circle defined by its radius. Test it using user input.
+
+// Here are some tips for solving this exercise:
+
+// Circumference and area calculation formulas should be part of your secondary school memories... Or a Google click away :)
+// The value of number π (Pi) is obtained with Math.PI in JavaScript.
+// You might want to use the exponentiation operator ** to perform computations.
+// console.log(2 ** 3); // 8: 2 * 2 * 2
+// console.log(3 ** 2); // 9: 3 * 3
+
+//circumfence of a circle 2pi r 
+function circumfenceOfCircle(r){
+    const circumfenceOfCircle = 2*Math.PI*r
+    console.log(circumfenceOfCircle)
+}
+circumfenceOfCircle(4)
+
+//area of a circle pi r square
+const areaOfCircle = r => Math.PI*r**2
+
+console.log(areaOfCircle(4))
