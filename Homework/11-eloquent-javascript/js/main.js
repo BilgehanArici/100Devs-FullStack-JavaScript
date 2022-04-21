7dded1912a2c49619a966840cfd01236fa808e5e
Bilgehan Arici <bilgehanarici97@gmail.com>
2022-04-21 21:19:18 +0300

    eloquent js finished

--- Homework/11-eloquent-javascript/js/main.js
+++ Homework/11-eloquent-javascript/js/main.js
@@ -1,4 +1,4 @@
-// // triangle
+// triangle
 // let counter = "#"
 
 // for (i=1;i<=7;i++){
@@ -125,15 +125,132 @@
 // }
 
 // horn()
-debugger
-function power(base, exponent) {
-  if (exponent == 0) {
-    return 1;
-  } else {
-    return base * power(base, exponent - 1);
-  }
+
+//// recursion
+// debugger
+// function power(base, exponent) {
+//   if (exponent == 0) {
+//     return 1;
+//   } else {
+//     return base * power(base, exponent - 1);
+//   }
+// }
+
+// console.log(power(2, 4));
+
+
+// function findSolution(target) {
+//   function find(current, history) {
+//     if (current == target) {
+//       return history;
+//     } else if (current > target) {
+//       return null;
+//     } else {
+//       return find(current + 5, `(${history} + 5)`) ||
+//              find(current * 3, `(${history} * 3)`);
+//     }
+//   }
+//   return find(1, "1");
+// }
+
+// console.log(findSolution(24));
+// // → (((1 * 3) + 5) * 3)
+
+
+//// exercises
+
+// function min(n1, n2){
+//   if(n1<n2){
+//     return n1
+//   }else if(n1>n2){
+//     return n2
+//   }else{
+//     return n1
+//   }
+// }
+
+//   console.log(min(0, 10));
+//   // → 0
+//   console.log(min(0, -10));
+// // → -10
+
+////recursive even and odd
+
+// function isEven(n) {
+//   if (n > 0) {
+//     if (n % 2 == 0) {
+//       return true
+//     } else if (n % 2 != 0) {
+//       return false
+//     } else {
+//       return n
+//     }
+//   }
+//   else{
+//     return "?"
+//   }
+// }
+
+
+// console.log(isEven(50));
+// // → true
+// console.log(isEven(75));
+// // → false
+// console.log(isEven(-1));
+// // → ??
+
+////bean counter
+
+
+//Your function will need a loop that looks at every character in the string. It can run an index from zero to one below its length (< string.length). If the character at the current position is the same as the one the function is looking for, it adds 1 to a counter variable. Once the loop has finished, the counter can be returned.
+
+//Take care to make all the bindings used in the function local to the function by properly declaring them with the let or const keyword.
+
+
+// function countBs(string) {
+//   let counter = 0
+//   for (i = 0; i < string.length; i++) {
+//     if (string[i] == 'B') {
+//       counter += 1
+//     }
+//   }
+//   return counter
+// }
+
+// console.log(countBs("BBC"));
+// // → 2
+
+// function countChar(string, char) {
+//   let counter = 0
+//   for (i = 0; i < string.length; i++) {
+//     if (string[i] == char) {
+//       counter += 1
+//     }
+//   }
+//   return counter
+// }
+
+// console.log(countChar("kkakerlak", "k"));
+// // → 4
+
+// console.log('string'.length)
+// console.log('string'.charAt(0))
+
+
+// console.log('string'[0])
+
+// console.log('Hey'.startsWith('h')) 
+
+// console.log('      Bilgehan         Arici   '.trim())
+
+// console.log(Math.random() * 50)
+
+
+if (true){
+  let letVariable = 'hello world';
+  var varVariable = 'hello world';
+  amVariable = 'hello world';
 }
-
-console.log(power(2, 4));
-
-//
+console.log(varVariable);
+console.log(letVariable); 
+console.log(amVariable); 
