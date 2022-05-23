8bc53d58510a53e46fd158bef5353734627beb52
Bilgehan Arici <bilgehanarici97@gmail.com>
2022-05-23 23:23:02 +0300

    class 19 hw cont

--- Week9/class19/class19-materials/arrays-fight-to-the-finish/js/main.js
+++ Week9/class19/class19-materials/arrays-fight-to-the-finish/js/main.js
@@ -1,22 +1,23 @@
-//Create an array of movie titles. Loop through the array and each element to the h2.
-let movieTitles = ['movie1','movie2','movie3']
+//Create an array of movie titles. Loop through the array and add each element to the h2.
 
-for(let i =0;i<movieTitles.length;i++){
-    document.querySelector('h2').innerText += movieTitles[i]
+let movies = ["fight", "win","worldwide"]
+
+for(let i = 0; i < movies.length;i++){
+    document.querySelector('h2').innerText += movies[i]
 }
 
-//Create an array of numbers. Loop through the array and three to each number and replace the old number.
+//Create an array of numbers. Loop through the array and add three to each number and replace the old number.
 let nums = [10,20,30]
 
-nums.forEach((element, index) => {
-    nums[i] = item + 3
-}) 
+// nums.forEach((item, i) => {
+//     nums[i] = item + 3
+// })
 
-//Find the average of all the numbers from question three
+//Find the average of all the numbers from question two
 let sum = 0
 
-for(let i =0; i< nums.length; i++){
+for(let i = 0; i < nums.length; i++){
     sum = sum + nums[i]
 }
 
-console.log(sum/nums.length)
+console.log( sum / nums.length  )
--- Week9/class19/class19-materials/arrays-in-it-to-win-it/js/main.js
+++ Week9/class19/class19-materials/arrays-in-it-to-win-it/js/main.js
@@ -1,12 +1,10 @@
 //Create an array of movies with at least three movies.
-let moviesArr = ['avengers','inception','batman']
-
+let movies = ['In It TO Win It', 'Fight To The Finish', 'Worldwide Cheersmack']
 //Using the array from above, store the first movie in a variable
-let firstMovie = moviesArr[0]
-
+let firstMovie = movies[0]
 //Get the length of the original array and store it in a new variable
-let arrayLength = moviesArr.length
-console.log(arrayLength)
-
+let arrLength = movies.length
+console.log(arrLength)
 //Get the last element in that array and store it in a new variable. What if your array was really large and you didn't know the last index? Would your solution still work?
-let lastElement = moviesArr[moviesArr.length-1]
+
+let lastElement = movies[ movies.length - 1]
