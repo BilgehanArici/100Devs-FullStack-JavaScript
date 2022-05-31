8bcd30909035790130984c64aa4d7dcc76ff0f21
Bilgehan Arici <bilgehanarici97@gmail.com>
2022-05-31 13:37:38 +0300

    class 20 hw done

--- Week9/class20/class20-materials/arrays-worldwide-cheersmack/js/main.js
+++ Week9/class20/class20-materials/arrays-worldwide-cheersmack/js/main.js
@@ -1,10 +1,25 @@
 //Create a function that takes in a number. The function should return an array that contains every number from 1 to that number as seperate elements
 
-function oneToThatNumFor(num) {
-    newArr = []
-    for (i = 1; i <= num; i++) {
+function yelloMello(number){
+    let newArr = []
+    for(i=1;i<=number;i++){
         newArr.push(i)
     }
     return newArr
 }
-console.log(oneToThatNumFor(5))
+
+console.log(yelloMello(5))
+
+
+
+
+
+
+// function oneToThatNumFor(num) {
+//     newArr = []
+//     for (i = 1; i <= num; i++) {
+//         newArr.push(i)
+//     }
+//     return newArr
+// }
+// console.log(oneToThatNumFor(5))
