2cc8a20d3708abbeb103e5ad6fe53cd0d5c4a40e
Bilgehan Arici <bilgehanarici97@gmail.com>
2022-07-22 17:09:13 +0300

    class 21 hw cont

--- Homework/13-javascript-info/js/main.js
+++ Homework/13-javascript-info/js/main.js
@@ -333,3 +333,58 @@
 //   };
 // }
 
+// Map to names
+// importance: 5
+// You have an array of user objects, each one has user.name. Write the code that converts it into an array of names.
+
+// For instance:
+
+// let john = {name: 'John', age: 25}
+// let pete = {name: 'Pete', age: 30}
+// let mary = {name: 'Mary', age: 28}
+
+// let users = [john, pete, mary]
+
+// let names = users.map(element=> element.name)
+// console.log(names)
+
+// Map to objects
+// importance: 5
+// You have an array of user objects, each one has name, surname and id.
+
+// Write the code to create another array from it, of objects with id and fullName, where fullName is generated from name and surname.
+
+// For instance:
+
+// let john = { name: "John", surname: "Smith", id: 1 };
+// let pete = { name: "Pete", surname: "Hunt", id: 2 };
+// let mary = { name: "Mary", surname: "Key", id: 3 };
+
+// let users = [ john, pete, mary ];
+
+// let usersMapped = /* ... your code ... */
+
+/*
+usersMapped = [
+  { fullName: "John Smith", id: 1 },
+  { fullName: "Pete Hunt", id: 2 },
+  { fullName: "Mary Key", id: 3 }
+]
+*/
+
+// alert( usersMapped[0].id ) // 1
+// alert( usersMapped[0].fullName ) // John Smith
+// // So, actually you need to map one array of objects to another. Try using => here. There’s a small catch.
+
+let john = {name:'John', surname:'Smith', id:1}
+let pete = {name: 'Pete', surname: 'Hunt', id:2}
+let mary = {name:'Mary', surname: 'Key', id:3}
+
+let users = [john, pete, mary]
+
+usersMapped = users.map(element => ({
+    fullName: `${element.name} ${element.surname}`,
+    id: element.id
+}))
+
+console.log(usersMapped)
