928373fe156fe4d2cd025c0a0269a5578eeb924b
Bilgehan Arici <bilgehanarici97@gmail.com>
2022-08-03 10:55:44 +0300

    class 21 is FINALLY finished

--- Homework/13-javascript-info/js/main.js
+++ Homework/13-javascript-info/js/main.js
@@ -376,15 +376,261 @@ usersMapped = [
 // alert( usersMapped[0].fullName ) // John Smith
 // // So, actually you need to map one array of objects to another. Try using => here. There’s a small catch.
 
-let john = {name:'John', surname:'Smith', id:1}
-let pete = {name: 'Pete', surname: 'Hunt', id:2}
-let mary = {name:'Mary', surname: 'Key', id:3}
+// let john = { name: "John", surname: "Smith", id: 1 };
+// let pete = { name: "Pete", surname: "Hunt", id: 2 };
+// let mary = { name: "Mary", surname: "Key", id: 3 };
 
-let users = [john, pete, mary]
+// let users = [john,pete,mary]
 
-usersMapped = users.map(element => ({
-    fullName: `${element.name} ${element.surname}`,
-    id: element.id
-}))
+// let usersMapped = users.map(element=>({
+//     fullName : `${element.name} ${element.surname}`,
+//     id: element.id
+// }))
 
-console.log(usersMapped)
+// console.log(usersMapped)
+// alert( usersMapped[0].id ); // 1
+// alert( usersMapped[0].fullName ); // John Smith
+
+// Sort users by age
+// importance: 5
+// Write the function sortByAge(users) that gets an array of objects with the age property and sorts them by age.
+
+// For instance:
+
+// function sortByAge(arr){
+//     arr.sort((a,b)=>a.age-b.age)
+// }
+
+//   let john = { name: "John", age: 25 };
+//   let pete = { name: "Pete", age: 30 };
+//   let mary = { name: "Mary", age: 28 };
+  
+//   let arr = [ pete, john, mary ];
+  
+//   sortByAge(arr);
+  
+//   // now sorted is: [john, mary, pete]
+//   alert(arr[0].name); // John
+//   alert(arr[1].name); // Mary
+//   alert(arr[2].name); // Pete
+
+// Shuffle an array
+// importance: 3
+// Write the function shuffle(array) that shuffles (randomly reorders) elements of the array.
+
+// Multiple runs of shuffle may lead to different orders of elements. For instance:
+
+
+// All element orders should have an equal probability. For instance, [1,2,3] can be reordered as [1,2,3] or [1,3,2] or [3,1,2] etc, with equal probability of each case.
+
+// function shuffle(array) {
+//   for (let i = array.length - 1; i > 0; i--) {
+//     let j = Math.floor(Math.random() * (i + 1));
+//     [array[i], array[j]] = [array[j], array[i]];
+//   }
+// }
+
+// function shuffle(array){
+//   for(let i = array.length-1;i>0;i--){
+//     let j = Math.floor(Math.random()*(i+1))
+//     [array[i],array[j]] = [array[j],array[i]]
+//   }
+// }
+
+
+// let arr = [1, 2, 3];
+
+// shuffle(arr);
+// console.log(arr)
+// // arr = [3, 2, 1]
+
+// shuffle(arr);
+// console.log(arr)
+// // arr = [2, 1, 3]
+
+// shuffle(arr);
+// console.log(arr)
+// // arr = [3, 1, 2]
+// // ...
+
+
+// //array shuffle => fisher-yates shuffle
+
+// Get average age
+// importance: 4
+// Write the function getAverageAge(users) that gets an array of objects with property age and returns the average age.
+
+// The formula for the average is (age1 + age2 + ... + ageN) / N.
+
+// For instance:
+
+// function getAverageAge(array){
+//   let totalAge = 0
+//   array.forEach(element => {
+//     totalAge+=element.age  
+//   });
+//   return totalAge/arr.length
+// }
+
+// let john = { name: "John", age: 25 };
+// let pete = { name: "Pete", age: 30 };
+// let mary = { name: "Mary", age: 29 };
+
+// let arr = [ john, pete, mary ];
+
+// alert( getAverageAge(arr) ); // (25 + 30 + 29) / 3 = 28
+
+// Filter unique array members
+// importance: 4
+// Let arr be an array.
+
+// Create a function unique(arr) that should return an array with unique items of arr.
+
+// For instance:
+
+// function unique(arr){
+//   uniqueArr = []
+//   for(let element of arr){
+//     if(!uniqueArr.includes(element)){
+//       uniqueArr.push(element)
+//     }
+//   }
+//   return uniqueArr
+// }
+
+// let strings = ["Hare", "Krishna", "Hare", "Krishna",
+//   "Krishna", "Krishna", "Hare", "Hare", ":-O"
+// ];
+
+// alert( unique(strings) ); // Hare, Krishna, :-O
+
+// Create keyed object from array
+// importance: 4
+// Let’s say we received an array of users in the form {id:..., name:..., age:... }.
+
+// Create a function groupById(arr) that creates an object from it, with id as the key, and array items as values.
+
+// For example:
+
+// let users = [
+//   {id: 'john', name: "John Smith", age: 20},
+//   {id: 'ann', name: "Ann Smith", age: 24},
+//   {id: 'pete', name: "Pete Peterson", age: 31},
+// ];
+
+// let usersById = groupById(users);
+
+/*
+// after the call we should have:
+
+usersById = {
+  john: {id: 'john', name: "John Smith", age: 20},
+  ann: {id: 'ann', name: "Ann Smith", age: 24},
+  pete: {id: 'pete', name: "Pete Peterson", age: 31},
+}
+*/
+
+// Hello, object
+// importance: 5
+// Write the code, one line for each action:
+
+// // Create an empty object user.
+// // Add the property name with the value John.
+// // Add the property surname with the value Smith.
+// // Change the value of the name to Pete.
+// // Remove the property name from the object.
+// // solution
+
+// let user = {}
+// user.name = 'John'
+// user.surname = 'Smith'
+// user.name = 'Pete'
+// delete user.name
+
+// console.log(user)
+
+// // Check for emptiness
+// // importance: 5
+// // Write the function isEmpty(obj) which returns true if the object has no properties, false otherwise.
+
+// // Should work like that:
+
+// // let schedule = {};
+
+// // alert( isEmpty(schedule) ); // true
+
+// // schedule["8:30"] = "get up";
+
+// // alert( isEmpty(schedule) ); // false
+
+// function isEmpty(object){
+//   for(key in object){
+//     //if the loop started there is a property
+//     return false
+//   }
+//   //if the loop hasnt started it is empty
+//   return true
+// }
+
+// let schedule = {}
+// schedule ['8:30'] = 'getup'
+
+// console.log(isEmpty(schedule)) // false
+
+// // Sum object properties
+// // importance: 5
+// // We have an object storing salaries of our team:
+
+// // let salaries = {
+// //   John: 100,
+// //   Ann: 160,
+// //   Pete: 130
+// // }
+// // Write the code to sum all salaries and store in the variable sum. Should be 390 in the example above.
+
+// // If salaries is empty, then the result must be 0.
+// let salaries = {
+//   John: 100,
+//   Ann: 160,
+//   Pete: 130
+// }
+
+// let sum =0
+// for(key in salaries){
+//   sum+= salaries[key]
+// }
+// console.log(sum)
+
+// Multiply numeric property values by 2
+// importance: 3
+// Create a function multiplyNumeric(obj) that multiplies all numeric property values of obj by 2.
+
+// For instance:
+
+// before the call
+let menu = {
+  width: 200,
+  height: 300,
+  title: "My menu"
+};
+
+function multiplyNumericValueOfObject(object){
+  for(key in object ){
+    if(typeof object[key] == 'number'){
+      object[key] *=2
+    }
+  }
+}
+
+multiplyNumericValueOfObject(menu)
+console.log(menu)
+
+// // after the call
+// menu = {
+//   width: 400,
+//   height: 600,
+//   title: "My menu"
+// };
+// Please note that multiplyNumeric does not need to return anything. It should modify the object in-place.
+
+// P.S. Use typeof to check for a number here.
