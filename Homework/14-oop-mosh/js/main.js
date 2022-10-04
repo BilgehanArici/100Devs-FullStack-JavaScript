619576afb4d249e673dba929a47557ec166738b8
Bilgehan Arici <bilgehanarici97@gmail.com>
2022-10-05 00:00:08 +0300

    class 22 hw cont

--- Homework/14-oop-mosh/js/main.js
+++ Homework/14-oop-mosh/js/main.js
@@ -1,14 +1,59 @@
-const circle = {
-    radius: 1,
-    location: {
-        x:1,
-        y:1
-    },
-    draw: function(){
-        console.log('draw')
-    }
-}
+// // factory function
+// function createCircle(radius){
+//     return {
+//         radius,
+//         draw: function() {
+//             console.log('draw')
+//         }
+//     }
+// }
 
-function createCircle(){
+// const circle = createCircle(1)
+
+// // constructor function
+// function Circle(radius){
+//     this.radius = radius
+//     this.draw = function(){
+//         console.log('draw')
+//     }
+// }
+// const another = new Circle(1)
+
+// let x = {}
+// let x = new Object()
+
+function Stopwatch(){
+    let startTime, endTime, running, duration = 0
+
+    this.start = function(){
+        if (running)
+            throw new Error('Stopwatch has already started')
+
+        running = true
     
+        startTime = new Date()
+    }
+
+    this.stop = function(){    
+        if (!running)
+            throw new Error('Stopwatch is not started')
+
+        running = false
+
+        endTIme = newDate()
+
+        const seconds = (endTime.getTime() - startTime.getTime()) / 1000
+        duration += seconds
+    }
+
+    this.reset = function(){
+        startTime = null
+        endTime = null
+        running = false
+        dutation = 0
+    }
+
+    Object.defineProperty(this, 'duration', {
+        get: function() {return duration}
+    })
 }
